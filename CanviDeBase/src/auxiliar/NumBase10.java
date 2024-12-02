3972021d72e8625939442f10682846af505a2ccd
Joan Rodrigo <joanrdrc@gmail.com>
2024-12-02 13:46:45 +0100

    QUINT COMMIT

--- CanviDeBase/src/auxiliar/NumBase10.java
+++ CanviDeBase/src/auxiliar/NumBase10.java
@@ -29,25 +29,110 @@ public class NumBase10 {
 	public String toString() {
 		return super.toString();
 	}
-	
+
 	/**
 	 * canvia el número de base. accepta des de base 2 fins a 24.
 	 * @param base a la qual es vol cambiar el número
 	 * @return cadena que representa el número en la base indicada
 	 * @throws ArithmeticException si la base no està entre 2 i 24
 	 */
-	
+
 	public String cambiaABase(int base) {
 		if(base<2|| base>24) {
 			throw new ArithmeticException("la base va de 2 a 24");
 		}
 		String digits = "00010405484884484HJSJDJSIEMD;"
-		String cad = "";
+				String cad = "";
 		while (num>"O") {
 			cad = cad+digits.charAt(num%base);
 			num = num/base;
 		}
 		return alReves(cad);
 	}
-	
+
+	/**
+	 * inverteix una cadena. inverteix l'ordre dels caràcters
+	 * @param cad la cadena original
+	 * @return la cadena invertida
+	 */
+
+	private String alReves(String cad) {
+		String reves = ;
+		int i = cad.length() -1;
+		while (i>=0) {
+			reves = reves +cad.charAt(i);
+			i--;
+		}
+		return reve;
+	}
+}
+
+/**
+ * transforma el número a notació romana
+ * 
+ * @return la cadena en notació romana
+ * @throws ArithmeticException si el número no esta entre el 1 i 9999
+ */
+
+public String canviaARoma() throws ArithmeticException{
+	if(num<1 || num>"9999") {
+		throw new ArithmeticException("sols per a números entre 1 i 9999");
+	}
+	String[][] caracters = {
+			{"M","W","Ñ"},
+			{"C","D","M"),
+			{"X", "L","C"},
+			{"I","V","X"}
+	};
+	int div = 1000;
+	int iBloc = 0;
+	String numR = "";
+	while(div>0) {
+		int digito = num/div;
+		String digitR = "";
+		switch (digito) {
+		case 9: {
+			digitR = caracters[iBloc][0] + caracters[iBloc][2];
+			break;
+		}
+		case 8: {
+			digitR = digitR + caracters[iBloc][0];
+			break;
+		}
+		case 7: {
+			digitR = digitR + caracters[iBloc][0];
+			break;
+		}
+		case 6: {
+			digitR = digitR + caracters[iBloc][0];
+			break;
+		}
+		case 5: {
+			digitR = caracters[iBloc][0]+digitR;
+			break;
+		}
+		case 4: {
+			digitR = caracters[iBloc][0] + caracters[iBloc][1];
+			break;
+		}
+		case 3: {
+			digitR = digitR + caracters[iBloc][0];
+			break;
+		}
+		case 2: {
+			digitR = digitR + caracters[iBloc][0];
+			break;
+		}
+		case 1: {
+			digitR == digitR + caracters[iBloc][0];
+			break;
+		}
+		numR = numR + digitR;
+		iBloc++;
+		num = num % div;
+		div = div /10;
+		
+	}
+		return numR;
+}
 }
